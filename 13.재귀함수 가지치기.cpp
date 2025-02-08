53ebd5ed022b6bbf77f1d2ad61cdc55d239d5c25
KKJ1000 <kkj1733n@gmail.com>
2025-02-08 22:31:56 +0900

    13.재귀함수 가지치기

---
+++ 13.재귀함수 가지치기.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+char path[5] = "";
+int visited[5] = {};
+
+void test(int level)
+{
+	if (level == 3)
+	{
+		std::cout << path << std::endl;
+		return;
+	}
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (visited[i] == 1)
+		{
+			continue;
+		}
+
+		visited[i] = 1;
+		path[level] = 'A' + i;
+		test(level + 1);
+		path[level] = 0;
+		visited[i] = 0;
+	}
+}
+
+
+int main()
+{
+	test(0);
+    return 0;
+}
